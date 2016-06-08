4fa772aafcbb6a44d2a684700f5281b7e62c3d65
maldon <maldonmeehan@gmail.com>
2016-06-07 17:03:13 -0700

    update js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $(function(){
   var flavors = ["chocolate", "coffee", "vanilla", "strawberry"];
   $("button").click(function(){
-    flavors.forEach(function(flavor){
-      $("ul").append("<li>" + flavor + "</li>");
-    });
+    for (var index=0; index<flavors.length; index+=1){
+      $("ul").append("<li>" + flavors[index] + "</li>");
+    }
+    // flavors.forEach(function(flavor){
+    //   $("ul").append("<li>" + flavor + "</li>");
+    // });
   });
 });
